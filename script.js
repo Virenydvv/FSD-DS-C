3778f0b845b1f4587822210fa83ea3748584ec80
Virenydvv <vviren355@gmail.com>
2024-10-18 14:11:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// function validate()
+// {
+//     let un=document.getElementById("un").value;
+//     let pass=document.getElementById("pass").value;
+//     if(un=='admin' && pass=='admin')
+//       document.getElementById("res").innerHTML="login success";.
+//     else document.getElementById("res").innerHTML="login failed";
+    
+
+// }
+let x =12;
+console.log(x);
+let y=14, message='hello',z=12.34;
+let p=true;
+let obj={name:"viren",id:12};
+let a;
+let c=null;
+console.log(typeof c);
